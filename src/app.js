0af904f2f25da1f9c6625df294a90f2b7465c36d
Gabriel Perez <gabrielperez@Gabriels-MacBook-Air.local>
2017-04-30 18:02:46 -0300

    arreglando navbar

--- src/app.js
+++ src/app.js
@@ -37,11 +37,6 @@ class App extends React.Component {
       <nav>
          <div className="nav-wrapper">
            <a href="#" className="brand-logo">{weekday[nDay]}</a>
-           <ul id="nav-mobile" className="right hide-on-med-and-down">
-             <li><a href="sass.html">Sass</a></li>
-             <li><a href="badges.html">Components</a></li>
-             <li><a href="collapsible.html">JavaScript</a></li>
-           </ul>
          </div>
        </nav>
         <div className='container' >
