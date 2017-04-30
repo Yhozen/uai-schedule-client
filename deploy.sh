1951b621ba28dd2f9328ba7a1042c9841e18e82e
Gabriel Perez <gabrielperez@Gabriels-MacBook-Air.local>
2017-04-30 15:21:36 -0300

    tratando travis

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  cp lib/ out/ && npm run compile && cp build/ out/
+  npm install && cp lib/ out/ && npm run compile && cp build/ out/
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
